dd850bf3165176c2fd2f4e56452484162cb8ec2d
Ait-ko <konutaevatai7@gmail.com>
2023-12-05 22:25:52 +0600

    hw 6

--- app/src/main/java/com/example/taskapp/model/OnBoarding.kt
+++ app/src/main/java/com/example/taskapp/model/OnBoarding.kt
@@ -3,7 +3,7 @@ package com.example.taskapp.model
 import java.io.Serializable
 
 data class OnBoarding(
-    val image: String? = null,
+    val image: Int? = null,
     val title: String? = null,
     val description: String? = null
 ) : Serializable
